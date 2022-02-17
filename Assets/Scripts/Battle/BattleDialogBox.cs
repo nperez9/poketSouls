f212ab7bd587b056a4087d23aef9c7529f82488c
Nicolas Agustín Perez <nicolasperez95.a@gmail.com>
2022-02-17 15:04:03 -0300

    qwe

--- Assets/Scripts/Battle/BattleDialogBox.cs
+++ Assets/Scripts/Battle/BattleDialogBox.cs
@@ -10,6 +10,16 @@ namespace Battle
         [SerializeField] Text DialogBoxText = null;
         [SerializeField] int letterForSecond = 30;
 
+        [SerializeField] GameObject ActionBox = null;
+        [SerializeField] GameObject MovesBox = null;
+        [SerializeField] GameObject MoveInfoBox = null;
+
+        [SerializeField] List<Text> ActionsList;
+        [SerializeField] List<Text> MovesList;
+        [SerializeField] Text MovePPText = null;
+        [SerializeField] Text MoveTypeText = null;
+
+
         public void SetDialogBoxText(string content)
         {
             DialogBoxText.text = content;
@@ -24,6 +34,22 @@ namespace Battle
                 DialogBoxText.text += letter;
                 yield return new WaitForSeconds(1f / letterForSecond);
             }
+        }
+
+        public void SetEnabledDialogBox(bool enabled)
+        {
+            DialogBoxText.enabled = true;
+        }
+
+        public void SetEnabledActionBox(bool enabled)
+        {
+            ActionBox.SetActive(enabled);
+        }
+
+        public void SetEnabledMoveBox(bool enabled)
+        {
+            MoveInfoBox.SetActive(enabled);
+            MovesBox.SetActive(enabled);
         }
     }
 }
--- Assets/Scripts/Battle/BattleSystem.cs
+++ Assets/Scripts/Battle/BattleSystem.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Battle { 
+    public enum BattleStates { Start, PlayerAction, PlayerMove, EnemyMove, Busy };
     public class BattleSystem : MonoBehaviour
     {
+        // Units setting
         [SerializeField] BattleUnit playerUnit = null;
         [SerializeField] BattleHud playerHud = null;
-       
         [SerializeField] BattleUnit enemyUnit = null;
         [SerializeField] BattleHud enemyHud = null;
-
+        
         [SerializeField] BattleDialogBox battleDialogBox = null;
 
-
+        private BattleStates state = BattleStates.Start;
         private void Start()
+        {
+            StartCoroutine(SetupBattle());
+        }
+
+        public IEnumerator SetupBattle()
         {
             playerUnit.Setup();
             playerHud.SetData(playerUnit.Pokemon);
@@ -22,7 +29,31 @@ namespace Battle {
             enemyUnit.Setup();
             enemyHud.SetData(enemyUnit.Pokemon);
 
-            StartCoroutine(battleDialogBox.TypeDialog($"A wild {enemyUnit.Pokemon.Base.Name} appears!"));
+            yield return battleDialogBox.TypeDialog($"A wild {enemyUnit.Pokemon.Base.Name} appears!");
+            yield return new WaitForSeconds(1f);
+
+            PlayerAction();
+        }
+
+        private void PlayerAction()
+        {
+            state = BattleStates.PlayerAction;
+            StartCoroutine(battleDialogBox.TypeDialog("What you wanna do?"));
+            battleDialogBox.SetEnabledActionBox(true);
+        }
+
+        private void Update()
+        {
+             if (state == BattleStates.PlayerAction)
+            {
+                HandlePlayerAction();
+            }
+        }
+
+
+        private void HandlePlayerAction()
+        {
+
         }
     }
 }
